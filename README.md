935232cd2435d7294f113890d31923720be16ee0
Thomas Carette <tcarette@gmail.com>
2013-11-06 15:04:43 +0100

    Add anaGrasp_func.sh (see README.md for info)

--- README.md
+++ README.md
@@ -4,17 +4,30 @@ ASP-HS
 Atomic structure packages (ATSP2K, GRASP2K) helper scripts
 
 
-anaATSP2K_func.sh
------------------
-Short functions that return various floats from output assuming standard
+#### anaATSP2K_func.sh
+
+Short functions that return various floats from ATSP2K output assuming standard
 file names (<root>.<std extension>).
 
 * default usage:
     * argument 1: file root name.
     * argument 2 (for hfs and bpci): J1 value in format Int or Int/2.
-    * argument 3 (for hfs): J2 value in format Int or Int/2 (for hfsA and hfsB)
+    * argument 3 (for hfs): J2 value in format Int or Int/2
 * functions beginning with D: functions that compute differential effects (first - second)
     * argument 1: first state file root name.
     * argument 2: J value for first state (in format Int or Int/2.)
     * argument 3: second state file root name.
     * argument 4: J value for second state (in format Int or Int/2.)
+
+#### anaGRASP_func.sh
+
+Short functions that return various floats from GRASP2K output assuming standard
+file names (<root>.<std extension>).
+
+* default usage:
+    * argument 1: file root name.
+    * argument 2: J value in format Int or Int/2.
+    * argument 3: index of the level in the J block
+* functions beginning with D: functions that compute differential effects (first - second)
+    * argument 1-3: as above for first state
+    * argument 4-6: as above for second state
---
+++ anaGrasp_func.sh
@@ -0,0 +1,405 @@
+#!/bin/bash
+
+inp ()
+{
+	echo $1
+}
+
+isnumber ()
+{
+ok=0
+for i in "$@"
+do
+  if ! [[ "$i" =~ ^[-]?[0-9]+?([.][0-9]+)?$ ]]
+  then
+    ok=1
+    break
+  fi
+done
+return $ok
+}
+
+compM ()
+{
+if [ "$1" != "" ]
+then
+#  echo "${1}*3609.48" | sed -e 's/[D]+*/\*10\^/' |  bc -l
+  echo "${1}          " | sed -e 's/[D]+*/\*10\^/' |  bc -l
+fi
+}
+
+compF ()
+{
+if [ "$1" != "" ]
+then
+  echo "${1}*2.34965/3*29*2*3.1416" | sed -e 's/[D]+*/\*10\^/' |  bc -l
+fi
+}
+
+
+
+check ()
+{
+if [ "$1" == "" ]
+then
+ echo " # "
+else
+ echo $1
+fi
+}
+
+CONVstart ()
+{
+if [ -e $1.sc ]
+then
+  if $( grep "; this fails the\|Iteration" $1.sc | tail -1 | grep -q fails )
+  then
+     local str=$( grep -B1 "; this fails the\|Iteration" $1.sc | tail -2 | grep -B1 fails | head -1)
+     orb=$( echo $str | awk '{print $2}' )
+     local str=$( echo $str | awk '{print $5}' )
+     echo "${orb}:$str"
+  else
+     echo "OK?"
+  fi
+else echo '     -     '
+fi
+}
+
+CONVnorm ()
+{
+if [ -e $1.sc ]
+then
+  local str=$( grep -n "^Subshell" $1.sc | tail -1 )
+  local local n=$(( ${str%:*} + 2 ))
+
+  local str=$(head -$n $1.sc | tail -1)
+  local fstr=$( echo ${str} | gawk '{print $1}' )
+  max=0.0
+
+  while [[ ${str:2:2} =~ [0-9][s,p,d,f,g,h,i,k,l,m,n] ]] || [[ "${fstr}" == "START:" ]] || [[ "${fstr}" == "E(" ]]
+  do
+    if [[ "${fstr}" == "START:" ]]
+    then
+      local local n=$(( n+3 ))
+      if [ $( wc $1.sc | awk '{print $1}' ) -le $n ];then  break;fi
+      local str=$(head -$n $1.sc | tail -1)
+      local fstr=$( echo ${str} | gawk '{print $1}' )
+      continue
+    fi
+    if [[ "${fstr}" == "E(" ]]
+    then
+      local local n=$(( n+1 ))
+      if [ $( wc $1.sc | awk '{print $1}' ) -le $n ];then  break;fi
+      local str=$(head -$n $1.sc | tail -1)
+      local fstr=$( echo ${str} | gawk '{print $1}' )
+      continue
+    fi
+    new=$( echo $str | sed -e 's/[D]+*/\*10\^/g' | gawk '{ if($6>=0) {print $6} else {print "-("$6")" }}' | bc -l )
+
+#echo $new $max  $(echo "$new > $max" | bc -l )
+    if (( $(echo "$new > $max" | bc -l ) ))
+    then
+      orb=$( echo $str | awk '{print $1}' )
+      max=$new 
+    fi
+
+    local local n=$(( n+1))
+    if [ $( wc $1.sc | awk '{print $1}' ) -le $n ];then  break;fi
+    local str=$(head -$n $1.sc | tail -1)
+    local fstr=$( echo ${str} | gawk '{print $1}' )
+  done
+  local num=$(echo $max | gawk '{print $1*1}')
+  echo $orb:$num
+else echo '     -     '
+fi
+}
+
+CONVdpm ()
+{
+if [ -e $1.sc ]
+then
+  local str=$( grep -n "^Subshell" $1.sc | tail -1 )
+  local local n=$(( ${str%:*} + 2 ))
+
+  local str=$(head -$n $1.sc | tail -1)
+  max=0.0
+
+  while [[ ${str:2:2} =~ [0-9][s,p,d,f,g,h,i,k,l,m,n] ]] || [[ "${fstr}" == "START:" ]] || [[ "${fstr}" == "E(" ]]
+  do
+    if [[ "${fstr}" == "START:" ]] 
+    then
+      local local n=$(( n+3 ))
+      if [ $( wc $1.sc | awk '{print $1}' ) -le $n ];then  break;fi
+      local str=$(head -$n $1.sc | tail -1)
+      local fstr=$( echo ${str} | gawk '{print $1}' )
+      continue
+    fi
+    if [[ "${fstr}" == "E(" ]]
+    then
+      local local n=$(( n+1 ))
+      if [ $( wc $1.sc | awk '{print $1}' ) -le $n ];then  break;fi
+      local str=$(head -$n $1.sc | tail -1)
+      local fstr=$( echo ${str} | gawk '{print $1}' )
+      continue
+    fi
+
+    new=$( echo $str | sed -e 's/[D]+*/\*10\^/g' | gawk '{ if($5>=0) {print $5} else {print "-("$5")" }}' | bc -l )
+
+#echo $new $max  $(echo "$new > $max" | bc -l )
+    if (( $(echo "$new > $max" | bc -l ) ))
+    then
+      orb=$( echo $str | awk '{print $1}' )
+      max=$new
+    fi
+
+    local local n=$(( n+1))
+
+    if [ $( wc $1.sc | awk '{print $1}' ) -le $n ];then  break;fi
+
+    local str=$(head -$n $1.sc | tail -1)
+    local fstr=$( echo ${str} | gawk '{print $1}' )
+  done
+  local num=$(echo $max | gawk '{print $1*1}')
+  echo $orb:$num
+else echo '     -     '
+fi
+}
+
+ncfg ()
+{
+if [ -e $1.c ]
+then
+  rnrcfg $1.c
+fi
+}
+
+en ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.sum ]
+then
+  local str=$( awk '/Eigenenergies:/,/Weights of major contributors to ASF/' $1.sum | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$(echo "${str}" | sed -e 's/[D]+*/\*10\^/' |  bc -l)
+fi
+check $str
+}
+
+enc ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.csum ]
+then
+  local str=$( awk '/Eigenenergies:/,/Weights of major contributors to ASF/' $1.csum | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$(echo "${str}" | sed -e 's/[D]+*/\*10\^/' |  bc -l)
+fi
+check $str
+}
+
+whichmain ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.sum ]
+then
+  local str=$( awk '/Weights of major contributors to ASF/,0' $1.sum | grep "^ *$n *$2 *[-,+]\?" | head -2 | tail -1 | gawk '{ print $1 }' )
+fi
+check $str
+}
+
+mainconf ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.sum ]
+then
+  local str=$( awk '/  CSF contributions/,0' $1.sum | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $5 }' )
+  local str=$(echo "${str}" | sed -e 's/[D]+*/\*10\^/' |  bc -l)
+fi
+check $str
+}
+
+hfsA ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ch ]
+then
+  local str=$( grep -A50 "Interaction constants" $1.ch | grep "^ *$n *$2 [+,-]\?" | tail -1 | gawk '{ print $4 }' )
+  local str=$(echo "${str}" | sed -e 's/[D]+*/\*10\^/' |  bc -l)
+fi
+check $str
+}
+
+hfsB ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ch ]
+then
+  local str=$( grep -A50 "Interaction constants" $1.ch | grep "^ *$n *$2 [-,+]\?" | tail -1 | gawk '{ print $5 }' )
+  local str=$(echo "${str}" | sed -e 's/[D]+*/\*10\^/' |  bc -l)
+fi
+Check $str
+}
+
+pipj ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ci ]
+then
+  local str=$( awk '/Specific mass shift/,/Electron density in atomic units/' $1.ci | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$( compM $str)
+fi
+check $str
+}
+
+smsK ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ci ]
+then
+  local str=$( awk '/Specific mass shift/,/Electron density in atomic units/' $1.ci | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $6 }' )
+  local str=$( compM $str)
+fi
+check $str
+}
+
+nmsK ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ci ]
+then
+  local str=$( awk '/Normal mass shift/,/Specific mass shift/' $1.ci | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $6 }' )
+  local str=$( compM $str)
+fi
+check $str
+}
+
+pi2 ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ci ]
+then
+  local str=$( awk '/Normal mass shift/,/Specific mass shift/' $1.ci | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$( compM $str)
+fi
+check $str
+}
+
+T ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.sms ]
+then
+  local str=$( awk '/T \(a.u.\)/,/Radial expectationvalue/' $1.sms | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$( compM $str)
+fi
+check $str
+}
+
+msK ()
+{
+a=$( nmsK "$1" "$2" "$3" )
+b=$( smsK "$1" "$2" "$3" )
+if $( isnumber $a $b )
+then
+  local str=$(echo "$a + $b" | bc -l)
+fi
+check $str
+}
+
+nrmsK ()
+{
+a=$( pi2 "$1" "$2" "$3" ) 
+b=$( pipj "$1" "$2" "$3" ) 
+if $( isnumber $a $b )
+then
+  local str=$(echo "$a + $b" | bc -l)
+fi
+check $str
+}
+
+RCImsK ()
+{
+a=$( T "$1" "$2" "$3" ) 
+b=$( pipj "$1" "$2" "$3" ) 
+if $( isnumber $a $b )
+then
+  local str=$(echo "$a + $b" | bc -l)
+fi
+check $str
+}
+
+dens ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ci ]
+then
+  local str=$( awk '/Electron density in atomic units/,/Radial expectationvalue/'  $1.ci | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$(echo "${str}" | sed -e 's/[D]+*/\*10\^/' |  bc -l)
+fi
+check $str
+}
+
+F ()
+{
+local n=$( inp $3 '.' )
+if [ -e $1.ci ]
+then
+  local str=$( awk '/Electron density in atomic units/,/Radial expectationvalue/' $1.ci | grep "^ *$n *$2 [-,+]\?" | head -1 | gawk '{ print $4 }' )
+  local str=$( compF $str )
+fi
+check $str
+}
+###################
+
+Dencmk ()
+{
+a=$( enc "$1" "$2" "$3" )
+b=$( enc "$4" "$5" "$6" )
+if $( isnumber $a $b )
+then
+  local str=$(echo "219474.63*($a - $b)" | bc -l)
+fi
+check $str
+}
+
+Dencmkc ()
+{
+a=$( enc "$1" "$2" "$3")
+b=$( enc "$4" "$5" "$6")
+if $( isnumber $a $b )
+then
+  local str=$(echo "219474.63*($a - $b)" | bc -l)
+fi
+check $str
+}
+
+
+for f in en enc nrmsK msK nmsK pipj pi2 smsK dens T RCImsK F smsSL
+do
+
+eval "D$f ()
+{
+a=\$( $f \$1 \"\$2\" \$3 )
+b=\$( $f \$4 \"\$5\" \$6 )
+if \$( isnumber \$a \$b )
+then
+  local str=\$(echo \$a - \$b | bc -l)
+fi
+echo \$( check \$str )
+}"
+
+done
+
+DsmsSL ()
+{
+a=$( DmsK $1 "$2" $3 $4 "$5" $6 )
+#b=$( enc $1 "$2" $3 $4 "$5" $6 )
+if $( isnumber $a ) # $b )
+then
+#  b=$( compM $b )
+#  local str=$(echo "$a + ($b)" | bc -l)
+  local str=$(echo "$a + 506.27615765283109" | bc -l)
+fi
+check $str
+}
+
+
+
